fefd08f04b1832ef4ae538ac135d829cf53ae00f
Zainab <zainabnasser294@gmail.com>
2026-08-24 03:32:56 +0400

    Update settings.py

--- baseera_production/baseera_web/settings.py
+++ baseera_production/baseera_web/settings.py
@@ -185,7 +185,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
-
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
