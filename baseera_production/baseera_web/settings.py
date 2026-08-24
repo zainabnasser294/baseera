67e8164d2b0da831343fb4a0cac450c5b7a56a07
Zainab <zainabnasser294@gmail.com>
2026-08-24 13:24:46 +0400

    Update settings.py

--- baseera_production/baseera_web/settings.py
+++ baseera_production/baseera_web/settings.py
@@ -186,7 +186,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
