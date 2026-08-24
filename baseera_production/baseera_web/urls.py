01ed05d2a1eb8c8a6949a39726f3e046d0b1c603
Zainab <zainabnasser294@gmail.com>
2026-08-24 13:58:19 +0400

    Update urls.py

--- baseera_production/baseera_web/urls.py
+++ baseera_production/baseera_web/urls.py
@@ -1,6 +1,5 @@
 """
 URL configuration for baseera_web project.
-
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/6.0/topics/http/urls/
 Examples:
@@ -14,13 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("dashboard.urls")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + [
+    path("static/<path:path>", serve, {"document_root": settings.STATIC_ROOT}),
+    path("media/<path:path>", serve, {"document_root": settings.MEDIA_ROOT}),
+]
